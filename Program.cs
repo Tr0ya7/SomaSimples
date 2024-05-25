3ed13e9294bb6f7b123ed297e33b99583d36c141
Tr0ya7 <devluizhenrique@proton.me>
2024-05-25 19:30:01 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace App
+{
+    internal class Program
+    {
+        static int firstValue;
+        static int secondValue;
+
+        static void Main(string[] args)
+        {
+            GetFirstValue();
+            GetSecondValue();
+
+            int operationResult = firstValue + secondValue;
+
+            Console.WriteLine($"Resultado: {operationResult}");
+        }
+
+        /* static void ValidationValues(int var, Action methodName)         tentativa de previnir mensagens de erro padrão do programa
+        {
+            while (true)
+            {
+                try
+                {
+                    break;
+                }
+                catch (FormatException) //(var.GetType() != typeof(int) || firstValue == 0)
+                {
+                    methodName();
+                }
+            }
+        } */
+
+        static void GetFirstValue()
+        {
+            Console.Write("Digite o primeiro valor da soma: ");
+            firstValue = int.Parse(Console.ReadLine());
+
+            //ValidationValues(firstValue, GetFirstValue);
+        }
+
+        static void GetSecondValue()
+        {
+            Console.Write("Digite o segundo valor da soma: ");
+            secondValue = int.Parse(Console.ReadLine());
+
+            //ValidationValues(secondValue, GetSecondValue);
+        }
+    }
+}
